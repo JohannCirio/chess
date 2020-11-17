fb2c856a5ce499ca90cbbbfc1d570afeee2b38b7
Johann <johannocsantos@gmail.com>
2020-11-17 17:36:28 -0300

    Implements classes for all pieces, with the most basic functionalities and some basic tests for these same classes

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,7 +1,63 @@
+# implements the Pawn chess piece
 class Pawn
   attr_reader :color, :token, :current_column, :current_row
   def initialize(color, column, row)
     @token = '♟'
+    @color = color
+    @current_column = column
+    @current_row = row
+  end
+end
+
+# implements the Knight chess piece
+class Knight
+  attr_reader :color, :token, :current_column, :current_row
+  def initialize(color, column, row)
+    @token = '♞'
+    @color = color
+    @current_column = column
+    @current_row = row
+  end
+end
+
+# implements the Queen chess piece
+class Queen
+  attr_reader :color, :token, :current_column, :current_row
+  def initialize(color, column, row)
+    @token = '♚'
+    @color = color
+    @current_column = column
+    @current_row = row
+  end
+end
+
+# implements the Bishop chess piece
+class Bishop
+  attr_reader :color, :token, :current_column, :current_row
+  def initialize(color, column, row)
+    @token = '♝'
+    @color = color
+    @current_column = column
+    @current_row = row
+  end
+end
+
+# implements the Tower chess piece
+class Tower
+  attr_reader :color, :token, :current_column, :current_row
+  def initialize(color, column, row)
+    @token = '♝'
+    @color = color
+    @current_column = column
+    @current_row = row
+  end
+end
+
+# implements the King chess piece
+class King
+  attr_reader :color, :token, :current_column, :current_row
+  def initialize(color, column, row)
+    @token = '♝'
     @color = color
     @current_column = column
     @current_row = row
--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -12,18 +12,142 @@ describe Pawn do
 
     it 'its token is "♟"' do
       token = @pawn.token
-      expect(token).to eql("♟")
+      expect(token).to eql('♟')
     end
 
-    it "its initial column is H(0)" do
+    it 'its initial column is H(0)' do
       column = @pawn.current_column
       expect(column).to eql(0)
     end
 
-    it "its initial row is 1" do
+    it 'its initial row is 1' do
       row = @pawn.current_row
       expect(row).to eql(1)
     end
   end
+end
 
+describe Knight do
+  context 'white knight is correctly initialized in H1' do
+    before { @knight = Knight.new('white', 0, 1) }
+    it 'is white' do
+      color = @knight.color
+      expect(color).to eql('white')
+    end
+
+    it 'its token is ""' do
+      token = @knight.token
+      expect(token).to eql('♞')
+    end
+
+    it 'its initial column is H(0)' do
+      column = @knight.current_column
+      expect(column).to eql(0)
+    end
+
+    it 'its initial row is 1' do
+      row = @knight.current_row
+      expect(row).to eql(1)
+    end
+  end
+end
+
+describe Queen do
+  context 'white queen is correctly initialized in H1' do
+    before { @queen = Queen.new('white', 0, 1) }
+    it 'is white' do
+      color = @queen.color
+      expect(color).to eql('white')
+    end
+
+    it 'its token is ""' do
+      token = @queen.token
+      expect(token).to eql('♚')
+    end
+
+    it 'its initial column is H(0)' do
+      column = @queen.current_column
+      expect(column).to eql(0)
+    end
+
+    it 'its initial row is 1' do
+      row = @queen.current_row
+      expect(row).to eql(1)
+    end
+  end
+end
+
+describe Bishop do
+  context 'white bishop is correctly initialized in H1' do
+    before { @bishop = Bishop.new('white', 0, 1) }
+    it 'is white' do
+      color = @bishop.color
+      expect(color).to eql('white')
+    end
+
+    it 'its token is ""' do
+      token = @bishop.token
+      expect(token).to eql('♝')
+    end
+
+    it 'its initial column is H(0)' do
+      column = @bishop.current_column
+      expect(column).to eql(0)
+    end
+
+    it 'its initial row is 1' do
+      row = @bishop.current_row
+      expect(row).to eql(1)
+    end
+  end
+end
+
+describe Tower do
+  context 'white tower is correctly initialized in H1' do
+    before { @tower = Tower.new('white', 0, 1) }
+    it 'is white' do
+      color = @tower.color
+      expect(color).to eql('white')
+    end
+
+    it 'its token is ""' do
+      token = @tower.token
+      expect(token).to eql('♝')
+    end
+
+    it 'its initial column is H(0)' do
+      column = @tower.current_column
+      expect(column).to eql(0)
+    end
+
+    it 'its initial row is 1' do
+      row = @tower.current_row
+      expect(row).to eql(1)
+    end
+  end
+end
+
+describe King do
+  context 'white king is correctly initialized in H1' do
+    before { @king = King.new('white', 0, 1) }
+    it 'is white' do
+      color = @king.color
+      expect(color).to eql('white')
+    end
+
+    it 'its token is ""' do
+      token = @king.token
+      expect(token).to eql('♝')
+    end
+
+    it 'its initial column is H(0)' do
+      column = @king.current_column
+      expect(column).to eql(0)
+    end
+
+    it 'its initial row is 1' do
+      row = @king.current_row
+      expect(row).to eql(1)
+    end
+  end
 end
