c94b1e81c6f2ff7a7ecebcb0bc331a86b093c84c
Johann <johannocsantos@gmail.com>
2020-11-17 11:18:00 -0300

    Implement the #generate_board_matrix and #print_board methods.

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 # ./lib/board.rb
-
+require 'colorize'
+# generates the game board and manipulates it's contents
 class ChessBoard
   attr_reader :matrix
   def initialize
@@ -14,4 +15,59 @@ class ChessBoard
       @matrix.push(row_array)
     end
   end
+
+  def print_alternative_board
+    puts '╔═╤═╤═╤═╤═╤═╤═╤═╗'
+    print_line(7)
+    puts '╟─┼─┼─┼─┼─┼─┼─┼─╢'
+    print_line(6)
+    puts '╟─┼─┼─┼─┼─┼─┼─┼─╢'
+    print_line(5)
+    puts '╟─┼─┼─┼─┼─┼─┼─┼─╢'
+    print_line(4)
+    puts '╟─┼─┼─┼─┼─┼─┼─┼─╢'
+    print_line(3)
+    puts '╟─┼─┼─┼─┼─┼─┼─┼─╢'
+    print_line(2)
+    puts '╟─┼─┼─┼─┼─┼─┼─┼─╢'
+    print_line(1)
+    puts '╟─┼─┼─┼─┼─┼─┼─┼─╢'
+    print_line(0)
+    puts '╚═╧═╧═╧═╧═╧═╧═╧═╝'
+    puts ' h g f e d c b a'
+  end
+
+  def print_line(line_number)
+    print '║'
+    if line_number.odd?
+      8.times do |i|
+        if @matrix[line_number][i] == 0
+          i.even? ? (print "░".on_white) : (print " ")
+        else
+          print @matrix[line_number][i].token
+        end
+        print '│'
+      end
+    else
+      8.times do |i|
+        if @matrix[line_number][i] == 0
+          i.even? ? (print " ") : (print "░".on_white)
+        else
+          print @matrix[line_number][i].token
+        end
+        print '│'
+      end
+    end
+    puts "\b║#{line_number}"
+
+  end
 end
+
+#puts '║░│ │░│ │░│ │░│ ║'
+    #puts '║♖│♘│♗│♕│♔│♗│♘│♖║'
+    #puts '║♙│♙│♙│♙│♙│♙│♙│♙║'
+    #puts '║♜│♞│♝│♛│♚│♝│♞│♜║'
+    #puts '║♟│♟│♟│♟│♟│♟│♟│♟║'
+
+board = ChessBoard.new
+board.print_alternative_board
